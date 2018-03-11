cba3372485b5c90c691c9c27821e24fc7a06c745
caocongcong <916548030@qq.com>
2018-03-11 20:55:44 +0800

    fix

--- sdk-python/src/ecs/plot_data.py
+++ sdk-python/src/ecs/plot_data.py
@@ -1,20 +1,52 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+from collections import OrderedDict
 
-def plot_set_data(set_info, save_filename=None):        
+# 如果filename_path不存在，则创建目录
+def mkdir(filename_path):
+    file_dir = os.path.split(filename_path)[0]
+    if not os.path.isdir(file_dir):
+        os.makedirs(file_dir)
+
+# 检车info中有无某类型的vm
+def hasVmType(set_info, vm_type):
+    if not set_info.has_key(vm_type):
+        print "没有vm类型：" + vm_type
+        return False
+    
+# 绘制集合中所有虚拟机的信息
+def plot_set_data(set_info, save_filename=None):      
     dataframe = pd.DataFrame(set_info).fillna(0)
     dataframe.index = pd.to_datetime(dataframe.index)
     dataframe.plot(figsize=(16, 16), linestyle='--', marker='o', grid=True)
     if save_filename != None:
+        mkdir(save_filename)
         plt.savefig(save_filename)
     pass
 
+# 绘制集合中所有虚拟机的累计信息
 def plot_set_cumsum_data(set_info, save_filename=None):
     dataframe = pd.DataFrame(set_info).fillna(0)
     dataframe.index = pd.to_datetime(dataframe.index)
     dataframe = dataframe.cumsum()
     dataframe.plot(figsize=(16, 16), linestyle='-', marker='o', grid=True)
     if save_filename != None:
+        mkdir(save_filename)
         plt.savefig(save_filename)
-    pass
+        pass
+    
+# 绘制集合中，某个vm的信息
+def plot_set_single_vm_data(vm_type, set_info, save_filename=None):
+    if not hasVmType(set_info, vm_type):
+        return False
+    
+    plot_set_data({vm_type: set_info[vm_type]}, save_filename)
+    
+# 绘制集合中，某个vm的累计信息
+def plot_set_single_vm_cumsum_data(vm_type, set_info, save_filename=None):
+    if not hasVmType(set_info, vm_type):
+        return False
+    
+    plot_set_cumsum_data({vm_type: set_info[vm_type]}, save_filename)
