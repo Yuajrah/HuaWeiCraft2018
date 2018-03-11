d1a6f3ae2bf27efdbe0ec02e5c3247dc8a173b1a
DESKTOP-O6VHCR8\ruiy~~ <2997215859@qq.com>
2018-03-11 16:13:12 +0800

    plot_data.py里加入创建目录的函数，用于处理保存图片时目录不存在的情况

--- sdk-python/src/ecs/plot_data.py
+++ sdk-python/src/ecs/plot_data.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+
+def mkdir(filename_path):
+    file_dir = os.path.split(filename_path)[0]
+    if not os.path.isdir(file_dir):
+        os.makedirs(file_dir)
 
 def plot_set_data(set_info, save_filename=None):        
     dataframe = pd.DataFrame(set_info).fillna(0)
     dataframe.index = pd.to_datetime(dataframe.index)
     dataframe.plot(figsize=(16, 16), linestyle='--', marker='o', grid=True)
     if save_filename != None:
+        mkdir(save_filename)
         plt.savefig(save_filename)
     pass
 
@@ -16,5 +23,6 @@ def plot_set_cumsum_data(set_info, save_filename=None):
     dataframe = dataframe.cumsum()
     dataframe.plot(figsize=(16, 16), linestyle='-', marker='o', grid=True)
     if save_filename != None:
+        mkdir(save_filename)
         plt.savefig(save_filename)
-    pass
+        pass
