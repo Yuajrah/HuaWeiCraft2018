07a095c9464931ceb7594070e4e984b16556f812
DESKTOP-O6VHCR8\ruiy~~ <2997215859@qq.com>
2018-03-14 23:53:01 +0800

    保存代码

--- sdk-gcc/ecs/predict.cpp
+++ sdk-gcc/ecs/predict.cpp
@@ -10,11 +10,22 @@
  *   项目可执行文件的参数： "../../../data/exercise/date_2015_01_to_2015_05.txt" "../../../data/exercise/input_file.txt" resultFilePath = "../../../data/exercise/output_file.txt"
  * */
 //你要完成的功能总入口
+// info 是inputFile的数据，data是历史数据
+struct Server {
+	cores, // 核数
+	storage, // 内存大小
+	disk // 硬盘大小
+};
 void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int data_num, char * filename)
 {
+	Server server;
     /*int days = get_days("2013-03-15", "2013-03-16");
     printf("%d\n", days);*/
-
+    sscanf(info[0],"%d %d %d",&server.cores, &server.storage, &server.disk_size);
+    sscanf(info[2],"%d",&type_num);
+	for (int i=3;i<3+type_num;i++) {
+		sscanf(info[i],"flavor%d %d %d",&type,&cores,&day);
+	}
     test_ar();
 
 	// 需要输出的内容
