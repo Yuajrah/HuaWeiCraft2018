be3b5fefe075751ac00b9c6fd86ef1911fbf9221
deadfish <916548030@qq.com>
2018-03-29 17:28:51 +0800

    改时间预测为监督学习

--- sdk-python/src/ecs/new_predict/huawei/split_data.py
+++ sdk-python/src/ecs/new_predict/huawei/split_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-def get_data(data, train_begin_date, test_begin_date, train_end_date, test_end_date):
+def get_data(data, train_begin_date, train_end_date,test_begin_date, test_end_date):
     target_ecs = ["flavor1","flavor2","flavor3","flavor4","flavor5","flavor6","flavor7","flavor8","flavor9","flavor10","flavor11","flavor12","flavor13","flavor14","flavor15"]
     #首先对数据进行聚合,统计每一种服务器们每天的个数
     groupby_data = data.groupby(["date","ecs"]).size().reset_index(name="count")
@@ -23,3 +23,22 @@ def get_data(data, train_begin_date, test_begin_date, train_end_date, test_end_d
         tmp_data = sum(test_data[ecs])
         test_target[ecs] = tmp_data
     return  train_data, test_data, test_target
+
+def series_to_supervised(data, split_windows):
+    data = pd.DataFrame(data)
+    cols = list()
+    names = list()
+    for i in range(split_windows+1):
+        cols.append(data.shift(i))
+        names += [('var(t-%d)' % (i))]
+    trian_data = pd.concat(cols, axis=1)
+    trian_data.columns = names
+    trian_data.dropna(inplace=True)
+    trian_data = trian_data.rename(index=str, columns={"var(t-0)": "target"})
+    target = trian_data["target"]
+    trian_data.drop('target',axis=1, inplace=True)
+    return trian_data, target
+
+def get_predict_score(data, split_windows, predict_mothod):
+    train , test = series_to_supervised(data , split_windows)
+    predict_mothod.fit(train, test)
