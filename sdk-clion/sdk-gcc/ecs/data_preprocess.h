fc6157ea58ee855469feb1a62505ef1224e9071d
ccc <916548030@qq.com>
2018-04-01 10:16:29 +0800

    -增加了移动平均的knn

--- sdk-clion/sdk-gcc/ecs/data_preprocess.h
+++ sdk-clion/sdk-gcc/ecs/data_preprocess.h
@@ -8,9 +8,12 @@
 #include "type_def.h"
 #include <vector>
 #include <map>
+#include "ma.h"
 //对于低频计算计算平均值
 double get_mean(std::vector<double> train_data, double rate);
 std::map<int, int> change_by_mean_vaule(std::map<int, std::vector<double>> train_data, std::map<int, Vm> vm_info, double rate, int need_predict_day,
-                            std::map<int, int> predict_data);
+                                        std::map<int, int> predict_data);
+std::map<std::vector<double>, double> timeseries_to_supervised(std::vector<double>, int split_windows, bool mv = false);
+std::vector<double>  get_frist_predict_data(std::vector<double>, int split_windows, bool mv = false);
 
 #endif //SDK_CLION_DATA_PREPROCESS_H
--- sdk-clion/sdk-gcc/ecs/ml_predict.cpp
+++ sdk-clion/sdk-gcc/ecs/ml_predict.cpp
@@ -9,13 +9,13 @@ std::map<int, int> predict_by_knn (std::map<int, Vm> vm_info, std::map<int, std:
     std::map<int,int>result;
     for (auto &t: vm_info) {
         std::vector<double> ecs_data = train_data[t.first];
-        std::map<std::vector<double>, double> train_data_need = timeseries_to_supervised(ecs_data, split_windows);
-        std::vector<double> frist_predict_data = get_frist_predict_data(ecs_data, split_windows);
+        std::map<std::vector<double>, double> train_data_need = timeseries_to_supervised(ecs_data, split_windows, true);
+        std::vector<double> frist_predict_data = get_frist_predict_data(ecs_data, split_windows, true);
 //        result[t.first] = 1;
         double ecs_sum = 0.0;
         for(int i=0; i < need_predict_day; i++)
         {
-            double tmp_predict = knn_regresion_brust(train_data_need,frist_predict_data, 10);
+            double tmp_predict = knn_regresion_brust(train_data_need,frist_predict_data, 5);
             frist_predict_data.erase(frist_predict_data.begin());
             frist_predict_data.push_back(tmp_predict);
             ecs_sum += tmp_predict;
