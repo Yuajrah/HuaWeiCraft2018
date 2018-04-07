035eed5e75d5b7acb6b1e602aa371a63282dba6d
ruiy <2997215859@qq.com>
2018-04-07 21:42:43 +0800

    修改一些代码

--- sdk-clion/sdk-gcc/ecs/predict.cpp
+++ sdk-clion/sdk-gcc/ecs/predict.cpp
@@ -251,59 +251,59 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
      */
 
 
-//    std::vector<std::map<int,int>> packing_result = packing(BasicInfo::vm_info, BasicInfo::server_info, predict_data, opt_object);
-//    std::vector<Bin> bins;
-//    int cnt = 0;
-//    for (auto &server: packing_result) {
-//        Bin bin(BasicInfo::server_info.core, BasicInfo::server_info.mem);
-//        for (auto &vm: server) {
-//            Vm t_vm = BasicInfo::vm_info[vm.first];
-//            for (int i=0;i<vm.second;i++) {
-//                t_vm.no = cnt++;
-//                t_vm.type = vm.first;
-//                bin.put(t_vm);
-//            }
-//        }
-//        bins.push_back(bin);
-//    }
+    std::vector<std::map<int,int>> packing_result = packing(BasicInfo::vm_info, BasicInfo::server_info, predict_data, BasicInfo::opt_object);
+    std::vector<Bin> bins;
+    int cnt = 0;
+    for (auto &server: packing_result) {
+        Bin bin(BasicInfo::server_info.core, BasicInfo::server_info.mem);
+        for (auto &vm: server) {
+            Vm t_vm = BasicInfo::vm_info[vm.first];
+            for (int i=0;i<vm.second;i++) {
+                t_vm.no = cnt++;
+                t_vm.type = vm.first;
+                bin.put(t_vm);
+            }
+        }
+        bins.push_back(bin);
+    }
 
 
 
-//    std::vector<Vm> objects = serialize(predict_data);
-//    int pop_size = 100;
-//    int cross_num = 40;
-//    double p_mutation = 0.15;
-//    int mutation_num = 5;
-//    int inversion_num = 10;
-//    int iter_num = 8000;
-//    GGA gga(objects, pop_size, cross_num, p_mutation, mutation_num, inversion_num, iter_num);
-////    gga.initial(bins, 100);
+    std::vector<Vm> objects = serialize(predict_data);
+    int pop_size = 100;
+    int cross_num = 40;
+    double p_mutation = 0.15;
+    int mutation_num = 5;
+    int inversion_num = 10;
+    int iter_num = 8000;
+    GGA gga(objects, pop_size, cross_num, p_mutation, mutation_num, inversion_num, iter_num);
+    gga.initial(bins, 100);
 //    gga.initial({}, 0);
-//    gga.start();
-//    std::vector<Bin> allocate_result = gga.get_best_chrome().get_bin();
-//
-//    std::vector<std::pair<int, Vm>> order_vm_info(BasicInfo::vm_info.begin(), BasicInfo::vm_info.end());
-//    std::sort(order_vm_info.begin(), order_vm_info.end(), [](const std::pair<int, Vm>& a, const std::pair<int, Vm>& b) {
-//        return a.second.mem > b.second.mem;
-//    });
-//    after_process(allocate_result, order_vm_info, predict_data);
-//    std::string result2 = format_allocate_res(allocate_result);
+    gga.start();
+    std::vector<Bin> allocate_result = gga.get_best_chrome().get_bin();
+
+    std::vector<std::pair<int, Vm>> order_vm_info(BasicInfo::vm_info.begin(), BasicInfo::vm_info.end());
+    std::sort(order_vm_info.begin(), order_vm_info.end(), [](const std::pair<int, Vm>& a, const std::pair<int, Vm>& b) {
+        return a.second.mem > b.second.mem;
+    });
+    after_process(allocate_result, order_vm_info, predict_data);
+    std::string result2 = format_allocate_res(allocate_result);
 
     /**
      * 第六版分配, 目前坠吼
      * 对最后的分配结果进行进一步的处理, 填充新的服务器
      */
-    std::vector<std::map<int,int>> packing_result = packing(BasicInfo::vm_info, BasicInfo::server_info, predict_data, BasicInfo::opt_object);
-    std::vector<Bin> allocate_result = vector_res_to_bins_res(packing_result);
-
-    std::vector<std::pair<int, Vm>> order_vm_info(BasicInfo::vm_info.begin(), BasicInfo::vm_info.end());
-
-    std::sort(order_vm_info.begin(), order_vm_info.end(), [](const std::pair<int, Vm>& a, const std::pair<int, Vm>& b) {
-        return a.second.mem > b.second.mem;
-    });
-
-    after_process(allocate_result, order_vm_info, predict_data);
-    std::string result2 = format_allocate_res(allocate_result);
+//    std::vector<std::map<int,int>> packing_result = packing(BasicInfo::vm_info, BasicInfo::server_info, predict_data, BasicInfo::opt_object);
+//    std::vector<Bin> allocate_result = vector_res_to_bins_res(packing_result);
+//
+//    std::vector<std::pair<int, Vm>> order_vm_info(BasicInfo::vm_info.begin(), BasicInfo::vm_info.end());
+//
+//    std::sort(order_vm_info.begin(), order_vm_info.end(), [](const std::pair<int, Vm>& a, const std::pair<int, Vm>& b) {
+//        return a.second.mem > b.second.mem;
+//    });
+//
+//    after_process(allocate_result, order_vm_info, predict_data);
+//    std::string result2 = format_allocate_res(allocate_result);
 
     /**
      * 第六版分配
